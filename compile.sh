b496be2ab33d5ea679a5c713be51703fa7c403e1
Wenqing Zong <wenqing.zong98@gmail.com>
2025-01-27 20:50:07 +0000

    Faster compile

--- compile.sh
+++ compile.sh
@@ -3,17 +3,16 @@
 BUILD_DIR="build"
 if [ -d "$BUILD_DIR" ]; then
     echo "Removing existing build directory..."
-    rm -rf "$BUILD_DIR"
+    rm -rf "$BUILD_DIR"/*
 fi
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
 START_TIME="$SECONDS"
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake --fresh
-
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake --fresh || exit 1
 # To get avoid of make's Clock skew waring.
 sleep 2
-make
+make -j$(nproc) || exit 1
 END_TIME="$SECONDS"
 ELAPSED=$((END_TIME - START_TIME))
 
