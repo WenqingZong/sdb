6d50bf41bb7fee1d9993a5205b98da5ecbc9a5d5
Wenqing Zong <wenqing.zong98@gmail.com>
2025-03-23 23:37:02 +0000

    Add test for variable operations, but member pointer test failed

--- test/targets/global_variable.cpp
+++ test/targets/global_variable.cpp
@@ -7,3 +7,22 @@ int main() {
 
     return 0;
 }
+
+struct cat {
+    const char* name;
+    int age : 5;
+    int color : 3;
+};
+
+struct person {
+    const char* name;
+    int age;
+    cat* pets;
+    int num_pets;
+};
+
+cat marshmallow{"Marshmallow", 4, 1};
+cat lexical_cat{"Lexical Cat", 8, 2};
+cat milkshake{"Milkshake", 4, 3};
+cat cats[] = {marshmallow, lexical_cat, milkshake};
+person sy{"Sy", 33, cats, 3};
