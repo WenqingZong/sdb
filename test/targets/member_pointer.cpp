450a367603b643a6a8ef98719b43f755faee0700
Wenqing Zong <wenqing.zong98@gmail.com>
2025-03-24 00:29:45 +0000

    So, a comment can truely change test behaviour, but why?

--- test/targets/member_pointer.cpp
+++ test/targets/member_pointer.cpp
@@ -6,14 +6,7 @@ struct cat {
 };
 
 int main() {
-    // Define a member pointer called data_ptr that can point to any data member
-    // of cat whose type is const char*, and initialize it to point to
-    // cat::name.
     const char*(cat::*data_ptr) = &cat::name;
-
-    // A member function pointer called func_ptr that can point to any member
-    // function of cat so long as it takes no arguments and returns void. We
-    // initialize the pointer to cat::meow.
     void (cat::*func_ptr)() const = &cat::meow;
 
     cat marshmallow{"Marshmallow"};
